757cbe3500950aa81c6673c2f5e802b9c682d87a
web前端-曾佑森 <2356281422@qq.com>
2017-11-20 08:46:15 +0800

    代码更续

--- react_first/src/index.js
+++ react_first/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import ClickCounter from './ClickCounter';
+// import ClickCounter from './ClickCounter';
 import ControlPanel from './ControlPanel';
 import registerServiceWorker from './registerServiceWorker';
 
